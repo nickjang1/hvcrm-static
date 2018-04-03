8800a65d7faa2c0b5a5abe5d1ba0635f318c5b9b
nickjang1 <eunhyukjang1@gmail.com>
2018-04-03 12:03:02 +0700

    google analytics

--- index.php
+++ index.php
@@ -16,7 +16,17 @@
   <link href="css/style.css" rel="stylesheet">
 
   <!-- Styles required by this views -->
+  <!-- Google Analytics -->
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+    ga('create', 'UA-116856008-1', 'auto');
+    ga('send', 'pageview');
+  </script>
+  <!-- End Google Analytics -->
 </head>
 
 <body class="app flex-row align-items-center">
